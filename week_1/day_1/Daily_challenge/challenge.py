fa06bd22fe4fbebe40fd3211903b1265b7510bfe
soufiane amabrra <manga.sfn@gmail.com>
2025-09-12 20:35:21 +0100

    daily challenge

---
+++ week_1/day_1/Daily_challenge/challenge.py
@@ -0,0 +1,33 @@
+#Challenge 1
+""""
+while True:
+
+    try:
+        number = int(input("Enter a number pleas: "))
+        length =int(input("Enter the length of the list: "))
+        print(f"the number is :{number} and the length is :{length}")
+        break
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
+
+list_of_numbers = []
+for i in range(length):
+    if not list_of_numbers:
+        list_of_numbers.append(number)
+    else:
+        list_of_numbers.append(list_of_numbers[-1] + number)
+print(f"the list is :{list_of_numbers}")  
+
+"""
+
+#Challenge 2
+
+word = input("Enter a word: ")
+
+result = word[0] 
+for char in word:
+    if char != result[-1]:
+        result += char
+
+
+print(f"{result}")
